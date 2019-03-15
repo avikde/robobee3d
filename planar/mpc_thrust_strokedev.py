b2db6cdbd02d34679ebc72b258bb9a7abaf1d767
Avik De <avikde@gmail.com>
2019-03-14 20:34:29 -0500

    modularized into a function

--- planar/mpc_thrust_strokedev.py
+++ planar/mpc_thrust_strokedev.py
@@ -7,35 +7,7 @@ import FlappingModels
 
 np.set_printoptions(precision=2, suppress=True, linewidth=100)
 
-'''
-Learnings about parameters:
-- tuning parameters: weights, N, dt, eps_abs, eps_rel
-- longer N => problem harder to solve => infeasible result more likely
-- shorter N and/or longer dt => instability more likely
-- eps_i lower => infeasible more likely (TODO: needs more testing)
-'''
-
-# Sim parameters
-nsim = 100
-N = 20 #horizon
-dt = 0.002
-# control types
-CTRL_LIN_CUR = 0
-CTRL_LIN_HORIZON = 1
-CTRL_OPEN_LOOP = 2
-ctrlType = CTRL_LIN_CUR
-
 model = FlappingModels.PlanarThrustStrokeDev()
-mpc = MPCUtils.MPCHelper(model, N, [0.01, 0.01, 1, 0, 0, 0, 0], [1, 10000], verbose=False, eps_abs=1e-08, eps_rel=1e-08)
-
-# Initial and reference states
-# x0 = 0.01 * np.random.rand(model.nx)
-x0, ctrl = model.getInit()
-
-# Simulate in closed loop
-X = np.zeros((nsim, model.nx))
-U = np.zeros((nsim, model.nu))
-firstA = 0
 
 # Trajectory following?
 def getXr(t):
@@ -53,35 +25,70 @@ def getXr(t):
 	# 	xr = np.array([0.0, 0.1, 0.5 * np.pi * (t - 0.1),0.,0.,0.])
 
 	return xr
+
+def runMPCSim(wx, wu, N=20, dt=0.002, epsi=1e-6):
+	'''
+	N = horizon (e.g. 20)
+	dt = timestep (e.g. 0.002)
 	
-t = np.arange(nsim) * dt
-desTraj = np.zeros((nsim,3))
+	Learnings about parameters:
+	- tuning parameters: weights, N, dt, eps_abs, eps_rel
+	- TODO: reason about units for wxi, wui and reduce those to two scalars?
+	- longer N => problem harder to solve => infeasible result more likely
+	- shorter N and/or longer dt => instability more likely
+	- eps_i lower => infeasible more likely (TODO: needs more testing)
+	'''
+	# Sim parameters
+	nsim = 100
+	# control types
+	CTRL_LIN_CUR = 0
+	CTRL_LIN_HORIZON = 1
+	CTRL_OPEN_LOOP = 2
+	ctrlType = CTRL_LIN_CUR # could make this a param
 
-for i in range(nsim):
-	# tgoal = (i + N) * dt
-	tgoal = (i + 1) * dt
-	xr = getXr(tgoal)
-	# for logging
-	desTraj[i,:] = xr[0:3]
+	mpc = MPCUtils.MPCHelper(model, N, wx, wu, verbose=False, eps_abs=epsi, eps_rel=epsi)
 
-	if ctrlType == CTRL_LIN_CUR:
-		ctrl = mpc.update(x0, np.zeros(2), xr, dt)
-	elif ctrlType == CTRL_LIN_HORIZON:
-		# traj to linearize around
-		x0horizon = np.zeros((N,model.nx))
-		for xi in range(model.nx):
-			x0horizon[:,xi] = np.linspace(x0[xi], xr[xi], N, endpoint=False)
-		ctrl = mpc.update(x0horizon, np.tile(np.zeros(2),(N,1)), xr, dt)
-	else: # openloop
-		ctrl = np.array([1e-6,0])
+	# Initial and reference states
+	# x0 = 0.01 * np.random.rand(model.nx)
+	x0, ctrl = model.getInit()
 
-	# simulate forward
-	Ad, Bd = model.getLin(x0, ctrl, dt)
-	x0 = Ad.dot(x0) + Bd.dot(ctrl)
-	print(i, x0, ctrl)
-	# print(x0horizon)
-	X[i, :] = x0
-	U[i, :] = ctrl
+	# Simulate in closed loop
+	X = np.zeros((nsim, model.nx))
+	U = np.zeros((nsim, model.nu))
+	firstA = 0
+		
+	t = np.arange(nsim) * dt
+	desTraj = np.zeros((nsim,3))
+
+	for i in range(nsim):
+		# tgoal = (i + N) * dt
+		tgoal = (i + 1) * dt
+		xr = getXr(tgoal)
+		# for logging
+		desTraj[i,:] = xr[0:3]
+
+		if ctrlType == CTRL_LIN_CUR:
+			ctrl = mpc.update(x0, np.zeros(2), xr, dt)
+		elif ctrlType == CTRL_LIN_HORIZON:
+			# traj to linearize around
+			x0horizon = np.zeros((N,model.nx))
+			for xi in range(model.nx):
+				x0horizon[:,xi] = np.linspace(x0[xi], xr[xi], N, endpoint=False)
+			ctrl = mpc.update(x0horizon, np.tile(np.zeros(2),(N,1)), xr, dt)
+		else: # openloop
+			ctrl = np.array([1e-6,0])
+
+		# simulate forward
+		Ad, Bd = model.getLin(x0, ctrl, dt)
+		x0 = Ad.dot(x0) + Bd.dot(ctrl)
+		print(i, x0, ctrl)
+		# print(x0horizon)
+		X[i, :] = x0
+		U[i, :] = ctrl
+
+	return t, desTraj, X, U
+
+t, desTraj, X, U = runMPCSim([0.01, 0.01, 1, 0, 0, 0, 0], [1, 10000], N=20, dt=0.002, epsi=1e-8)
 
 # print(x0.shape)
 plt.subplot(3,1,1)
