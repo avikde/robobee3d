3e82567413f95841a39e4e0e0d4bfadaecb74896
Avik De <avikde@gmail.com>
2019-03-18 14:05:22 -0400

    planning out the interface of these models

--- planar/FlappingModels.py
+++ planar/FlappingModels.py
@@ -17,28 +17,34 @@ class PlanarThrustStrokeDev:
 	g = 9.81
 	ib = 1/12. * mb * l**2
 	d = 2e-3
+	# initial conditions
+	y0 = np.array([0,0,0,0,0,0,self.g])
+	u0 = np.array([0, 0])
 
-	def getLin(self, y, u, dt):
+	def __init__(self, dt):
+		self.dt = dt
+
+	def getLinearDynamics(self, y, u):
 		phi = y[2]
 		cphi0 = np.cos(phi)
 		sphi0 = np.sin(phi)
 
 		# derivatives of the continuous dynamics
 		# New: adding g as a state
-		All = dt / self.mb * np.array([
+		All = self.dt / self.mb * np.array([
 			[0,0,-(self.g * self.mb + u[0]) * cphi0/self.mb],
 			[0,0,-(self.g + u[0] / self.mb) * sphi0],
 			[0,0,0]
 		])
-		Bl = dt * np.array([
+		Bl = self.dt * np.array([
 			[-(sphi0/self.mb),0],
 			[cphi0/self.mb,0],
 			[u[1]/self.ib,(self.g * self.mb + u[0])/self.ib]
 		])
 		nq = All.shape[0]
 
-		Aupper = np.hstack([np.eye(nq), dt * np.eye(nq), np.zeros((nq,1))])
-		Alower = np.hstack([All, np.eye(nq), dt * np.array([[-sphi0],[-1 + cphi0],[self.mb * u[1]/self.ib]])])
+		Aupper = np.hstack([np.eye(nq), self.dt * np.eye(nq), np.zeros((nq,1))])
+		Alower = np.hstack([All, np.eye(nq), self.dt * np.array([[-sphi0],[-1 + cphi0],[self.mb * u[1]/self.ib]])])
 		Ag = np.array([0,0,0,0,0,0,1])
 		Ad = np.vstack([Aupper, Alower, Ag])
 		Bd = np.vstack([np.zeros((nq,2)), Bl, np.zeros((1,2))])
@@ -53,9 +59,10 @@ class PlanarThrustStrokeDev:
 		xmin = np.array([-np.inf,-np.inf,-10*np.pi,-np.inf,-np.inf,-np.inf,-np.inf])
 		xmax = -xmin
 		return umin, umax, xmin, xmax
-	
-	def getInit(self):
-		return np.array([0,0,0,0,0,0,self.g]), np.array([0, 0])
+
+	def getReference(self, N):
+		ref = RefTraj(N, self.dt)
+		raise 'TODO'
 
 	nx = 7
 	nu = 2
