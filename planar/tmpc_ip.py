f9c616f3577f892657f58f0b603a775c766b89a1
Avik De <avikde@gmail.com>
2019-04-17 17:38:25 -0400

    visualize value function

--- planar/tmpc_ip.py
+++ planar/tmpc_ip.py
@@ -8,6 +8,7 @@ from autograd import jacobian
 import sys
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 sys.path.append('..')
 from controlutils.py import lqr
@@ -36,18 +37,32 @@ Aup = Afun(yup)
 # LQR
 Q = np.eye(2)
 R = np.eye(1)
-K, X = lqr.lqr(Aup, B, Q, R)
+K, P = lqr.lqr(Aup, B, Q, R)
 
 # Simulation
 y0 = np.array([2,0.0]) 
 sol = solve_ivp(lambda t, y: pendulum(y, K @ (yup - y)), [0, 10], y0, dense_output=True)
 t, y = sol.t, sol.y
 
+# visualize value function
+def f(x1,x2):
+    # quadratic
+    val = P[0,0] * x1**2 + P[1,1] * x2**2 + (P[0,1] + P[1,0]) * x1*x2
+    return val
+
+xx, yy = np.meshgrid(np.linspace(-np.pi, np.pi, 30), np.linspace(-10, 10, 30))
+zz = f(xx, yy)
+
 # Display -------------------
 
 fig, ax = plt.subplots(2)
 
 ax[0].plot(t, y[0,:])
-ax[1].plot(t, y[1,:])
+ax[1].contourf(xx, yy, zz, cmap='gray')
+
+# # plot
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.plot_surface(xx,yy,zz, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
 
 plt.show()
