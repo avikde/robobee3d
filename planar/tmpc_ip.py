200d88d5100b1bc7cdde5983b9f88496723365a2
Avik De <avikde@gmail.com>
2019-04-18 08:50:32 -0400

    use autolin

--- planar/tmpc_ip.py
+++ planar/tmpc_ip.py
@@ -25,24 +25,20 @@ doublePendulum = DoublePendulum()
 
 # auto linearization
 yup = np.array([np.pi, 0.0])
-uup = 0.0
-Afun = jacobian(lambda y: pendulum.dynamics(y, uup))
-Bfun = jacobian(lambda u: pendulum.dynamics(yup, u))
-B = Bfun(uup)[:,np.newaxis]
-Aup = Afun(yup)
+uup = np.array([0.0])
+A, B, c = pendulum.autoLin(yup, uup)
 # LQR
-Q = np.eye(2)
-R = np.eye(1)
-K, P = lqr.lqr(Aup, B, Q, R)
+K1, P1 = lqr.lqr(A, B, Q=np.eye(2), R=np.eye(1))
 
 # Simulation
 tf = 5.0
 dt = 0.01
 t_eval = np.arange(0, tf, dt)
 y0 = np.array([2,0.0])
-sol = solve_ivp(lambda t, y: pendulum.dynamics(y, K @ (yup - y)), [0, tf], y0, dense_output=True, t_eval=t_eval)
+sol = solve_ivp(lambda t, y: pendulum.dynamics(y, K1 @ (yup - y)), [0, tf], y0, dense_output=True, t_eval=t_eval)
 
 # double pendulum with MPC controller
+yup2 = np.array([np.pi, np.pi, 0, 0])
 N = 5
 wx = np.full(4, 1)
 wu = np.full(2, 0.01)
@@ -53,16 +49,20 @@ doublePendulum.limits = -umax, umax, -xmax, xmax
 # Instantiate MPC
 dpmpc = mpc.LTVMPC(doublePendulum, N, wx, wu, verbose=False, polish=False, scaling=0, eps_rel=1e-2, eps_abs=1e-2, kdamping=0)
 
-def mpcDoublePendulum(t,y):
+def mpcDoublePendulum(t, y):
     """Closed-loop dynamics with MPC controller"""
-    return doublePendulum.dynamics(y, np.zeros(2))
+    # umpc = dpmpc.update(y, yup2, costMode=mpc.FINAL)
+    # print(umpc.shape)
+    umpc = np.zeros(2)
+
+    return doublePendulum.dynamics(y, umpc)
 
 # simulate
 y02 = np.array([1,-1, 0, 0])
 sol2 = solve_ivp(mpcDoublePendulum, [0, tf], y02, dense_output=True, t_eval=t_eval)
 
 # visualize value function
-def lqrValueFunc(x1,x2):
+def lqrValueFunc(x1, x2, P):
     # quadratic
     val = P[0,0] * (x1 - yup[0])**2 + P[1,1] * x2**2 + (P[0,1] + P[1,0]) * (x1 - yup[0])*x2
     return val
@@ -78,7 +78,7 @@ ax[0].plot(sol2.t, sol2.y[0,:], label='dp0')
 ax[0].plot(sol2.t, sol2.y[1,:], label='dp1')
 ax[0].legend()
 
-ax[1].contourf(xx, yy, lqrValueFunc(xx, yy), cmap='gray_r')
+ax[1].contourf(xx, yy, lqrValueFunc(xx, yy, P1), cmap='gray_r')
 ax[1].plot(yup[0], yup[1], 'r*')
 
 # animation
