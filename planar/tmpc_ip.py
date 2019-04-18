5ae4f22307471393911eaf14549815db357eb9a4
Avik De <avikde@gmail.com>
2019-04-18 12:43:37 -0400

    mpc controller sort of working for dp

--- planar/tmpc_ip.py
+++ planar/tmpc_ip.py
@@ -44,30 +44,55 @@ uup2 = np.zeros(2)
 # LQR
 A, B, c = pendulum2['model'].autoLin(yup2, uup2)
 K2, pendulum2['P'] = lqr.lqr(A, B, Q=np.eye(4), R=0.01*np.eye(2))
+# Simulate
+y02 = np.array([0.2, 0.3, 0, 0])
+pendulum2['sol'] = solve_ivp(lambda t, y: pendulum2['model'].dynamics(y, K2 @ (yup2 - y)), [0, tf], y02, dense_output=True, t_eval=t_eval)
 
 # MPC
-N = 5
-wx = np.full(4, 1)
+N = 10
+wx = np.full(4, 100000)
 wu = np.full(2, 0.01)
 # add model limits here by munging
 umax = np.full(2, np.inf)
 xmax = np.full(4, np.inf)
 pendulum2['model'].limits = -umax, umax, -xmax, xmax
 # Instantiate MPC
+pendulum2['model'].dt = 0.2
 dpmpc = mpc.LTVMPC(pendulum2['model'], N, wx, wu, verbose=False, polish=False, scaling=0, eps_rel=1e-2, eps_abs=1e-2, kdamping=0)
 
-def mpcDoublePendulum(t, y):
-    """Closed-loop dynamics with MPC controller"""
-    # umpc = dpmpc.update(y, yup2, costMode=mpc.FINAL)
-    # print(umpc.shape)
-    umpc = np.zeros(2)
-
-    return pendulum2.dynamics(y, umpc)
-
 # simulate
-y02 = np.array([0.2, 0.3, 0, 0])
-pendulum2['sol'] = solve_ivp(lambda t, y: pendulum2['model'].dynamics(y, K2 @ (yup2 - y)), [0, tf], y02, dense_output=True, t_eval=t_eval)
-# sol2 = solve_ivp(mpcDoublePendulum, [0, tf], y02, dense_output=True, t_eval=t_eval)
+# need to have discrete changes to the MPC
+def discretizationEvent(t, y):
+    return t - tev[-1] - pendulum2['model'].dt
+discretizationEvent.direction = 1
+discretizationEvent.terminal = True
+
+# initial states
+tev = np.zeros(1)
+uev = np.zeros((1, 2))
+yev = np.zeros((1, 4))
+yev[0, :] = y02
+uev[0, :] = dpmpc.update(y02, yup2)
+pendulum2['tt'] = np.zeros(0)
+pendulum2['yy'] = np.zeros((0, 4))
+
+while True:
+    sol = solve_ivp(lambda t, y: pendulum2['model'].dynamics(y, uev[-1, :]), [tev[-1], tf], yev[-1, :], dense_output=True, events=discretizationEvent, t_eval=np.arange(tev[-1], tf - 1e-3, dt))
+
+    if sol.status == 1:
+        # events
+        tev = np.hstack((tev, sol.t_events[0][0]))
+        yev = np.vstack((yev, sol.sol(tev[-1])))
+        uev = np.vstack((uev, dpmpc.update(yev[-1, :], yup2)))
+        # continuous for plotting
+        pendulum2['tt'] = np.hstack((pendulum2['tt'], sol.t))
+        pendulum2['yy'] = np.vstack((pendulum2['yy'], sol.y.T))
+        # should we go again?
+        if tf - tev[-1] < dt:
+            break
+
+    if sol.status == 0:
+        break
 
 """Acrobot ----------------------------
 """
@@ -94,8 +119,11 @@ xx, yy = np.meshgrid(np.linspace(0, 2*np.pi, 30), np.linspace(-10, 10, 30))
 
 fig, ax = plt.subplots(3)
 
+print(pendulum2['yy'].shape)
+
 ax[0].plot(pendulum['sol'].t, pendulum['sol'].y[0, :], label='sp')
 ax[0].plot(pendulum2['sol'].t, pendulum2['sol'].y[:2, :].T, label='dp')
+ax[0].plot(pendulum2['tt'], pendulum2['yy'][:, 0:2], label='dpmpc')
 ax[0].legend()
 
 ax[1].contourf(xx, yy, lqrValueFunc(xx, yy, pendulum['P']), cmap='gray_r')
@@ -104,7 +132,8 @@ ax[1].plot(yup[0], yup[1], 'r*')
 # animation
 line1, = ax[2].plot([], [], '.-', lw=2, label='sp')
 line2, = ax[2].plot([], [], '.-', lw=2, label='dp')
-line3, = ax[2].plot([], [], '.-', lw=2, label='acro')
+line3, = ax[2].plot([], [], '.-', lw=2, label='dpmpc')
+line4, = ax[2].plot([], [], '.-', lw=2, label='acro')
 patches = [line1, line2, line3]
 ax[2].set_aspect(1)
 ax[2].set_xlim((-2,2))
@@ -117,6 +146,7 @@ def _init():
     line1.set_data([], [])
     line2.set_data([], [])
     line3.set_data([], [])
+    line4.set_data([], [])
     return patches
 
 def _animate(i):
@@ -129,9 +159,13 @@ def _animate(i):
         p1, p2 = pendulum2['model'].kinematics(pendulum2['sol'].y[0:2, i])
         line2.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
 
+    if i < len(pendulum2['tt']):
+        p1, p2 = pendulum2['model'].kinematics(pendulum2['yy'][i, 0:2])
+        line3.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
+
     if i < len(acrobot['sol'].t):
         p1, p2 = acrobot['model'].kinematics(acrobot['sol'].y[0:2, i])
-        line3.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
+        line4.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
     return patches
 
 anim = animation.FuncAnimation(fig, _animate, init_func=_init, frames=len(pendulum2['sol'].t), interval=1000*dt, blit=True)
