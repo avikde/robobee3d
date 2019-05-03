8afd1befea6fc2fdec9559df24286c0be83d4596
Avik De <avikde@gmail.com>
2019-05-03 12:50:36 -0400

    lqr control of lip

--- planar/tmpc_lip.py
+++ planar/tmpc_lip.py
@@ -20,77 +20,68 @@ A, B, c = lip.autoLin(yup, uup)
 # LQR
 K1, P1 = lqr.lqr(A, B, Q=np.eye(2), R=0.01*np.eye(1))
 
-print(A)
+print(K1, P1)
 
-# # Simulation
-# tf = 5.0
-# dt = 0.01
-# t_eval = np.arange(0, tf, dt)
-# y0 = np.array([2,0.0])
-# pendulum['sol'] = solve_ivp(lambda t, y: pendulum['model'].dynamics(y, K1 @ (yup - y)), [0, tf], y0, dense_output=True, t_eval=t_eval)
+# Simulation
+tf = 1.0
+dt = 0.01
+t_eval = np.arange(0, tf, dt)
+y0 = np.array([2,0.0])
+lipsol = solve_ivp(lambda t, y: lip.dynamics(y, K1 @ (yup - y)), [0, tf], y0, dense_output=True, t_eval=t_eval)
 
-# # ---
+# make a list for display
+dispsols = [
+    {'name': 'lip', 'col': 'b', 'sol': lipsol, 'model': lip}
+]
 
-# # visualize value function
-# def lqrValueFunc(x1, x2, P):
-#     # quadratic
-#     val = P[0,0] * (x1 - yup[0])**2 + P[1,1] * x2**2 + (P[0,1] + P[1,0]) * (x1 - yup[0])*x2
-#     return val
+# ---
 
-# xx, yy = np.meshgrid(np.linspace(0, 2*np.pi, 30), np.linspace(-10, 10, 30))
+# visualize value function
+def lqrValueFunc(x1, x2, P):
+    # TODO: will need slices
+    val = P[0,0] * (x1 - yup[0])**2 + P[1,1] * x2**2 + (P[0,1] + P[1,0]) * (x1 - yup[0])*x2
+    return val
 
-# # Display -------------------
+xx, yy = np.meshgrid(np.linspace(0, 2*np.pi, 30), np.linspace(-10, 10, 30))
 
-# fig, ax = plt.subplots(3)
+# Display -------------------
 
-# ax[0].plot(pendulum['sol'].t, pendulum['sol'].y[0, :], label='sp')
-# ax[0].plot(pendulum2['sol'].t, pendulum2['sol'].y[:2, :].T, label='dp')
-# ax[0].plot(pendulum2['sol2'].t, pendulum2['sol2'].y[:2, :].T, label='dpmpc')
-# ax[0].plot(acrobot['sol2'].t, acrobot['sol2'].y[:2, :].T, label='acro')
-# ax[0].legend()
+fig, ax = plt.subplots(3)
+
+for dispsol in dispsols:
+    ax[0].plot(dispsol['sol'].t, dispsol['sol'].y[0, :], label=dispsol['name'])
+ax[0].legend()
 
 # ax[1].contourf(xx, yy, lqrValueFunc(xx, yy, pendulum['P']), cmap='gray_r')
-# ax[1].plot(yup[0], yup[1], 'r*')
+ax[1].plot(yup[0], yup[1], 'r*')
 
-# # animation
-# line1, = ax[2].plot([], [], '.-', lw=2, label='sp')
-# line2, = ax[2].plot([], [], '.-', lw=2, label='dp')
-# line3, = ax[2].plot([], [], '.-', lw=2, label='dpmpc')
-# line4, = ax[2].plot([], [], '.-', lw=2, label='acro')
-# patches = [line1, line2, line3, line4]
-# ax[2].set_aspect(1)
-# ax[2].set_xlim((-2,2))
-# ax[2].set_ylim((-2,2))
-# ax[2].grid(True)
-# ax[2].legend(bbox_to_anchor=(2,1))
-# plt.tight_layout()
+# animation
+patches = []
+for dispsol in dispsols:
+    linei, = ax[2].plot([], [], '.-', lw=2, label=dispsol['name'])
+    patches.append(linei)
+ax[2].set_aspect(1)
+ax[2].set_xlim((-0.3,0.3))
+ax[2].set_ylim((-0.3,0.3))
+ax[2].grid(True)
+ax[2].legend(bbox_to_anchor=(2,1))
+plt.tight_layout()
 
-# def _init():
-#     line1.set_data([], [])
-#     line2.set_data([], [])
-#     line3.set_data([], [])
-#     line4.set_data([], [])
-#     return patches
+def _init():
+    for pp in patches:
+        pp.set_data([], [])
+    return patches
 
-# def _animate(i):
-#     # get the vertices of the pendulum
-#     if i < len(pendulum['sol'].t):
-#         p1 = pendulum['model'].kinematics(pendulum['sol'].y[0:1, i])
-#         line1.set_data([0, p1[0]], [0, p1[1]])
+def _animate(i):
+    # get the vertices of the pendulum
+    for mi in range(len(dispsols)):
+        dispsol = dispsols[mi]
+        if i < len(dispsol['sol'].t):
+            p1 = dispsol['model'].kinematics(dispsol['sol'].y[0:1, i])
+            patches[mi].set_data([0, p1[0]], [0, p1[1]])
 
-#     if i < len(pendulum2['sol'].t):
-#         p1, p2 = pendulum2['model'].kinematics(pendulum2['sol'].y[0:2, i])
-#         line2.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
+    return patches
 
-#     if i < len(pendulum2['sol2'].t):
-#         p1, p2 = pendulum2['model'].kinematics(pendulum2['sol2'].y[0:2, i])
-#         line3.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
+anim = animation.FuncAnimation(fig, _animate, init_func=_init, frames=len(lipsol.t), interval=1000*dt, blit=True)
 
-#     if i < len(acrobot['sol2'].t):
-#         p1, p2 = acrobot['model'].kinematics(acrobot['sol2'].y[0:2, i])
-#         line4.set_data([0, p1[0], p2[0]], [0, p1[1], p2[1]])
-#     return patches
-
-# anim = animation.FuncAnimation(fig, _animate, init_func=_init, frames=len(pendulum2['sol'].t), interval=1000*dt, blit=True)
-
-# plt.show()
+plt.show()
