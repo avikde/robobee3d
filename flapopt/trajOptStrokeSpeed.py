6f737f7eb9e1f45a00d4a291b6a8e3caeb96650f
Avik De <avikde@gmail.com>
2019-06-03 10:38:01 -0400

    new script

---
+++ flapopt/trajOptStrokeSpeed.py
@@ -0,0 +1,33 @@
+import autograd.numpy as np
+from autograd import jacobian
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import sys
+sys.path.append('..')
+import planar.FlappingModels as FlappingModels
+np.set_printoptions(precision=4, suppress=True, linewidth=200)
+
+
+
+m = FlappingModels.PlanarStrokeSpeed()
+
+# discrete => do not need solve_ivp
+
+yi = m.y0
+dt = 0.01
+u = np.array([1,1])
+tf = 0.1
+tvec = np.arange(0, tf, dt)
+yi = np.zeros((len(tvec), len(yi)))
+yi[0,:]
+
+for ti in range(1, len(tvec)):
+    yi[ti,:] = yi[ti-1,:] + m.dydt(yi[ti-1,:], u) * dt
+
+# display
+
+fig, ax = plt.subplots(2)
+
+ax[0].plot(tvec, yi[:,0])
+
+plt.show()
