75d8f830c047a7f9493230f1c351ae76b407d558
Avik De <avikde@gmail.com>
2019-06-14 11:15:28 -0400

    make notes of integrating EKF in the future

--- h7bee/app/ImuTask.cpp
+++ h7bee/app/ImuTask.cpp
@@ -1,19 +1,32 @@
-
+/**
+ * @file ImuTask.cpp
+ * @author Avik De (avikde@gmail.com)
+ * @brief Task for flap to flap control
+ * @version 0.1
+ * @date 2019-06-14
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
 #include "InvensenseIMU.h"
+// TODO: include Eigen
 
 // defined in main.c
-extern SPI_HandleTypeDef hspi2;
+extern "C" SPI_HandleTypeDef hspi2;
+extern "C" {
 // TODO: RTOS message passing
 float extest, eytest, eztest;
+}
 
-void startImuTask(void const *argument)
+extern "C" void startImuTask(void const *argument)
 {
 	InvensenseIMU imu;
 	int invres = invensenseIMUInit(&imu, &hspi2);
+	// TODO: EKF with Eigen
 
 	for (;;)
 	{
