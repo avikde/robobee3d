bbdb00d2857568d55e0a244613ff5b65c566adda
Avik De <avikde@gmail.com>
2019-07-24 14:56:56 -0400

    test this next

--- flapopt/trajOptWing2DOF.py
+++ flapopt/trajOptWing2DOF.py
@@ -43,23 +43,8 @@ def Jcostsol(solt, soly, params):
 tf = 0.1
 tvec = np.arange(0, tf, dt)
 y0 = np.array([1e-2, 0, 0, 0])
-
-# YY = np.array([[0.963506,  0.00615112,  0.00404816,  0.309899,  0.466694],
-#  [0.417588,  0.452104,    0.0708825,   0.269745,  0.796851],
-#  [0.808803,  0.0559587,   0.0637219,   0.174673,  0.607593],
-#  [0.548119,  0.397353,    0.61362,     0.538941,  0.166001]])
-# print(xmax[0], wingopt.params)
-# for i in range(5):
-#     print(strokePosControlVF(0, YY[:,i]))
-# sys.exit()
 # Sim of an openloop controller
 sol = solve_ivp(strokePosControlVF, [0,tf], y0, dense_output=True, t_eval=tvec)
-
-_, ax = plt.subplots(2)
-ax[0].plot(sol.t, sol.y[0,:])
-ax[1].plot(sol.t, sol.y[1,:])
-plt.show()
-sys.exit()
 # Decimate the rate to get a starting traj for optimization with fewer knot points
 # At this point there is a "nominal trajectory" in sol.y
 yu0 = sol.y.copy()
@@ -137,6 +122,9 @@ params0 = wingopt.params
 # traj0 = np.hstack((traj0, wingopt.params))
 _, r = wingopt.Jcosttraj_penalty(traj0, wpo.N, params0)
 lambda0 = np.zeros_like(r)
+
+print(traj0.shape, traj0, r)
+sys.exit()
 
 optavglift = {'mu': 1e-1, 'timestep': (1e3, 1e-4, 1e-2)}
 optavgliftparams = {'mu':1e-1, 'dynamics': 1e3, 'method':wpo.NEWTON_METHOD}
