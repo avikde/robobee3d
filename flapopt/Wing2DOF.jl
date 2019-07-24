6a28cea11d13cd52ae6cb552d3ee2bf0e909142d
Avik De <avikde@gmail.com>
2019-07-24 11:00:55 -0400

    fancier rewriting

--- flapopt/Wing2DOF.jl
+++ flapopt/Wing2DOF.jl
@@ -6,62 +6,44 @@ using ForwardDiff
 # nu = 1
 # y0 = np.zeros(nx)
 
-# rescale = 1.0
+rescale = 30
 
 """
 Returns aero force
 """
 function aero(y::Vector, u::Vector, _params::Vector)
-    cbar = _params[1]
-    T = _params[2]
     CLmax = 1.8
     CDmax = 3.4
     CD0 = 0.4
     rho = 1.225
     R = 15e-3
     
-    sigma = y[1] * T
-    psi = y[2]
-    dsigma = y[3] * T
-    dpsi = y[4]
-    cpsi = cos(psi)
-    spsi = sin(psi)
-    alpha = pi / 2 - psi
+    # unpack
+    cbar, T = _params
+    σ, Ψ, dσ, dΨ = [T, 1, T, 1] .* y
+    cpsi = cos(Ψ)
+    spsi = sin(Ψ)
+    α = π / 2 - Ψ # AoA
 
     # aero force
-    wing1 = [sigma, 0]
+    wing1 = [σ, 0]
     paero = wing1 + [cpsi -spsi; spsi cpsi] * [0, -cbar]
     Jaero = [1 cbar * cpsi; 0 cbar * spsi]
-    CL = CLmax * sin(2 * alpha)
-    CD = (CDmax + CD0)/2 - (CDmax - CD0)/2 * cos(2 * alpha)
-    vaero = [dsigma, 0]
+    CL = CLmax * sin(2 * α)
+    CD = (CDmax + CD0)/2 - (CDmax - CD0)/2 * cos(2 * α)
+    vaero = [dΨ, 0]
     # TODO: confirm and expose design params as argument
-    Faero = 1/2 * rho * cbar * R * dot(vaero, vaero) * [CD, CL] * sign(-dsigma)
+    Faero = 1/2 * rho * cbar * R * dot(vaero, vaero) * [CD, CL] * sign(-dΨ)
 
     return paero, Jaero, Faero
 end
 
-println("hi")
-y0 = [0.1,0.1,1,0]
-u0 = [0.]
-params0 = [0.05,1]
-# paero, Jaero, Faero = aero(y, u, params)
-paeroFun(q::Vector) = aero([q;[0,0]], u0, params0)[1]
-println("paero ", paeroFun(y0))
-
-# Try to use ForwardDiff
-JaeroFun = y -> ForwardDiff.jacobian(paeroFun, y)
-println(JaeroFun(y0[1:2]))
-println(aero(y0, u0, params0)[2])
-
-# function dydt(self, yin, u, _params=[], dydt)
-#     ''' 
-#     See mma file flapping wing traj
-#     '''
-#     cbar = _params[0]
-#     T = _params[1]
-#     Kscale = np.diag([self.rescale, 1, self.rescale, 1])
-#     y = np.linalg.inv(Kscale) @ yin
+# "Continuous dynamics"
+# function dydt(yin::Vector, u::Vector, _params::Vector)
+#     cbar = _params[1]
+#     T = _params[2]
+#     Kscale = Diagonal([rescale, 1, rescale, 1])
+#     y = inv(Kscale) * yin
 #     # NOTE: for optimizing transmission ratio
 #     # Thinking of y = (sigma_actuator, psi, dsigma_actuator, dpsi)
 #     # u = (tau_actuator)
@@ -95,6 +77,19 @@ println(aero(y0, u0, params0)[2])
 
 #     dydt = Kscale @ np.array([dsigma, dpsi, ddq[0], ddq[1]])
 # end
+
+println("hi")
+y0 = [0.1,0.1,1,0]
+println(Diagonal([2,1,2,1]) * y0)
+u0 = [0.]
+params0 = [0.05,1.0]
+paeroFun(q::Vector) = aero([q;[0,0]], u0, params0)[1]
+println("paero ", paeroFun(y0[1:2]))
+
+# Try to use ForwardDiff
+JaeroFun = y -> ForwardDiff.jacobian(paeroFun, y)
+# println(JaeroFun(y0[1:2]))
+println(aero(y0, u0, params0)[2])
 
 # @property
 # def limits(self):
