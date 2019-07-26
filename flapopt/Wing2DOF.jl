afeb383d7ba60c896a4b73c37fa84842958e99c7
Avik De <avikde@gmail.com>
2019-07-26 15:53:04 -0400

    hm should actually not have divided by δt

--- flapopt/Wing2DOF.jl
+++ flapopt/Wing2DOF.jl
@@ -190,7 +190,7 @@ end
 function cu.Jobj(m::Wing2DOFModel, traj::Vector, params::Vector; vart::Bool=true, fixedδt::Float64=1e-3)::Number
 	N = cu.Nknot(m, traj; vart=vart)
     liy, liu = cu.linind(m, N)
-	δt = vart ? traj[end] : fixedδt
+	# δt = vart ? traj[end] : fixedδt
     Favg = @SVector zeros(2)
     for k = 1:N
         vy = @view liy[:,k]
@@ -199,7 +199,7 @@ function cu.Jobj(m::Wing2DOFModel, traj::Vector, params::Vector; vart::Bool=true
         Favg += Faero
     end
     # Divide by the total time
-    Favg /= (N*δt) # [mN]
+    Favg /= (N) # [mN]
     # max avg lift
 	return -Favg[2]
 end
