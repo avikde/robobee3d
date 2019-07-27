0a0797a9c668739c5d368a7d55580e4374329097
Avik De <avikde@gmail.com>
2019-07-27 00:30:23 -0400

    finally no crash

--- flapopt/ModelOptimizer.jl
+++ flapopt/ModelOptimizer.jl
@@ -16,13 +16,6 @@ Note that the actual constraints are:
 """
 function gvalues!(gout::Vector, m::Model, traj::Vector, params::Vector; vart::Bool=true, fixedδt::Float64=1e-3, order::Int=1)
 	ny, nu = dims(m)
-	
-	# #FIXME: only try init cond
-	# gout[1:ny] = traj[1:ny]
-	# return
-	println("CALLED gvalues! with $(ny) $(nu) $(N) $(pointer_from_objref(traj))")
-
-
 	N = Nknot(m, traj; vart=vart)
 	liy, liu = linind(m, N)
 	δt = vart ? traj[end] : fixedδt
@@ -43,10 +36,6 @@ end
 # "Bounds corresponding to the constraints above"
 function gbounds(m::Model, traj::Vector; vart::Bool=true)::Tuple{Vector, Vector}
 	ny, nu = dims(m)
-
-	# #FIXME: only try init cond
-	# return traj[1:ny], traj[1:ny]
-
 	N = Nknot(m, traj; vart=vart)
 	println("CALLED gbounds with $(ny) $(nu) $(N) $(pointer_from_objref(traj))")
 	g_L = [-traj[1:ny]; -Inf*ones(N*ny)]
@@ -57,42 +46,22 @@ end
 
 function Dgnnz(m::Model, N::Int; vart::Bool=true)::Int
 	ny, nu = dims(m)
-	# FIXME:
-	# return ny
-	println("CALLED Dgnnz")
-
 	# Assuming the Jacobians are dense. The terms below correspond to the "-I"s, the "I + δt A"'s, the "δt B"'s
 	return ny*(N+1) + ny^2*N + ny*nu*N
 end
 
 function Dgsparse!(row::Vector{Int32}, col::Vector{Int32}, value::Vector, m::Model, traj::Vector, params::Vector, mode; vart::Bool=true, fixedδt::Float64=1e-3, order::Int=1)
 	ny, nu = dims(m)
-
-	# # FIXME: only try init cond
-	# if setVals
-	# 	for i = 1:ny
-	# 		value[i] = 1
-	# 	end
-	# else
-	# 	for i = 1:ny
-	# 		row[i] = col[i] = i
-	# 	end
-	# end
-	# return
-
-
 	N = Nknot(m, traj; vart=vart)
-	# FIXME: NOTE even including a print of traj[115] caused a crash here before this print
-	println("CALLED Dgsparse! with $(mode) $(size(row)) $(size(col)) $(size(value)) $(ny) $(nu) $(N) $(pointer_from_objref(traj))")
 	liy, liu = linind(m, N)
 
-	# FIXME: CRASH HERE unless hardcoded
-	δt = traj[115]#vart ? traj[end] : fixedδt
-	# Preallocate outputs
-	df_dy = zeros(ny, ny)
-	df_du = zeros(ny, nu)
-	
-	println("HI 1")
+	# NOTE: traj is NULL when in :Structure mode
+	if mode != :Structure
+		δt = vart ? traj[end] : fixedδt
+		# Preallocate outputs
+		df_dy = zeros(ny, ny)
+		df_du = zeros(ny, nu)
+	end
 
 	# Fill in -I's
 	for ii = 1:ny*(N+1)
@@ -103,22 +72,17 @@ function Dgsparse!(row::Vector{Int32}, col::Vector{Int32}, value::Vector, m::Mod
 			value[ii] = -1
 		end
 	end
-
-	println("HI 2")
 	
 	# Offsets into the row[], col[], val[] arrays. These will be incremented and keep track of the index in the loops below.
 	offsA = ny*(N+1)
 	offsB = ny*(N+1) + ny^2*N
-	
-	println("HI 3")
 
 	# Fill in Jacobians
 	for k = 1:N
-		# Get the jacobians at this y, u
-		# FIXME: CRASH HERE if the δt problem is bypassed
-		Df!(df_dy, df_du, m, traj[@view liy[:,k]], traj[@view liu[:,k]], params)
-	
-		println("HI 3.5")
+		if mode != :Structure
+			# Get the jacobians at this y, u
+			Df!(df_dy, df_du, m, traj[@view liy[:,k]], traj[@view liu[:,k]], params)
+		end
 
 		# Insert A
 		# NOTE: j outer loop for Julia's col-major storage and better loop unrolling
@@ -133,8 +97,6 @@ function Dgsparse!(row::Vector{Int32}, col::Vector{Int32}, value::Vector, m::Mod
 				end
 			end
 		end
-	
-		println("HI 4 $(k)")
 
 		# Insert B
 		for j = 1:nu
@@ -148,11 +110,7 @@ function Dgsparse!(row::Vector{Int32}, col::Vector{Int32}, value::Vector, m::Mod
 				end
 			end
 		end
-		
-		println("HI 5 $(k)")
 	end
-	
-	println("HI 6")
 	return
 end
 
@@ -161,7 +119,7 @@ Objective
 =========================================================================#
 
 function ∇Jobj!(∇Jout, m::Model, traj::Vector, params::Vector; vart::Bool=true)
-	println("CALLED DJobj with $(pointer_from_objref(traj))")
+	# println("CALLED DJobj with $(pointer_from_objref(traj))")
 	Jtraj(tt::Vector) = Jobj(m, tt, params; vart=vart)
 	ForwardDiff.gradient!(∇Jout, Jtraj, traj)
 	return
