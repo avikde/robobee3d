9a34f15f7a838e3a071bd20173f43f10411d4c03
Avik De <avikde@gmail.com>
2019-10-17 17:11:11 -0400

    no printf but UART data sending works - just use this for mavlink and forget about printf

--- g4bee/Src/stm32g4xx_it.c
+++ g4bee/Src/stm32g4xx_it.c
@@ -58,6 +58,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern UART_HandleTypeDef huart2;
 extern TIM_HandleTypeDef htim6;
 
 /* USER CODE BEGIN EV */
@@ -159,6 +160,20 @@ void DebugMon_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32g4xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles USART2 global interrupt / USART2 wake-up interrupt through EXTI line 26.
+  */
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
+
+  /* USER CODE END USART2_IRQn 1 */
+}
 
 /**
   * @brief This function handles TIM6 global interrupt, DAC1 and DAC3 channel underrun error interrupts.
--- g4bee/myapp/app.c
+++ g4bee/myapp/app.c
@@ -1,6 +1,9 @@
 
 #include "main.h"
 #include "cmsis_os.h"
+#include <stdio.h>
+
+extern UART_HandleTypeDef huart2;
 
 void startBlinkTask(void *argument)
 {
@@ -9,6 +12,8 @@ void startBlinkTask(void *argument)
 		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 		volatile uint32_t millis = HAL_GetTick();
 		// printf("hello %d\n", millis);
+		const char *_writeBuf = "hello";
+		HAL_UART_Transmit_IT(&huart2, _writeBuf, 5);
 		osDelay(100);
 	}
 }
