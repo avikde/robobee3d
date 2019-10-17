f26d34fe7769d6f18764e45ddcd0d60023c7918d
Avik De <avikde@gmail.com>
2019-10-17 19:10:47 -0400

    start at high

--- g4bee/Src/main.c
+++ g4bee/Src/main.c
@@ -314,7 +314,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(SS1_GPIO_Port, SS1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(SS1_GPIO_Port, SS1_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
