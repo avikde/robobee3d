cdf8f521677cbfdaad8ef75cd5b6ea86e7aa6222
Avik De <avikde@gmail.com>
2019-11-22 19:28:11 -0500

    lower stiffness

--- flapopt/run_w2d.jl
+++ flapopt/run_w2d.jl
@@ -18,11 +18,11 @@ includet("Wing2DOF.jl")
 # To get ma, use the fact that actuator resonance is ~1KHz => equivalent ma = 240/(2*pi)^2 ~= 6mg
 m = Wing2DOFModel(
 	17.0, # R, [Jafferis (2016)]
-	0.55#= 1.5 =#, #k output
+	0.1#= 1.5 =#, #k output
 	0, #b output
 	6, # ma
 	0, # ba
-	150#= 0 =#, # ka
+	15#= 0 =#, # ka
 	true) # bCoriolis
 ny, nu = cu.dims(m)
 param0 = [3.2,  # cbar[mm] (area/R)
