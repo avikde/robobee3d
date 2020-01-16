24a40263652a4784795afe6e81b6cdaef045c497
Avik De <avikde@gmail.com>
2020-01-15 20:32:40 -0500

    plot all the data

--- flapopt/transmission_static.jl
+++ flapopt/transmission_static.jl
@@ -1,18 +1,32 @@
-using DelimitedFiles, Plots
+using DelimitedFiles, Plots, LinearAlgebra
 
-fname = "data/transmission/poke1.csv"
-dat = readdlm(fname, ',', Float64, skipstart=2)
-# cols are t, Ax,Ay, Bx,By, Cx,Cy
-# Ax is the x pos of the actuator [mm]
-# B, C are points on the wing (arctan should be wing angle)
+fname = "poke1.csv"
 
-dat[:,2] .-= dat[1,2]
-Np = size(dat, 1)
-wingAngs = zeros(Np)
-for i=1:Np
-	wingVec = dat[i,6:7] - dat[i,4:5]
-	wingAngs[i] = atan(wingVec[2]/wingVec[1])
+function transmissionStaticProcessCSV(fname)
+	dat = readdlm(string("data/transmission/",fname), ',', Float64, skipstart=2)
+	# cols are t, Ax,Ay, Bx,By, Cx,Cy
+	# Ax is the x pos of the actuator [mm]
+	# B, C are points on the wing (arctan should be wing angle)
+
+	dat[:,2] .-= dat[1,2]
+	Np = size(dat, 1)
+	wingAngs = zeros(Np)
+	for i=1:Np
+		wingVec = dat[i,6:7] - dat[i,4:5]
+		wingAngs[i] = atan(wingVec[2]/wingVec[1])
+	end
+
+	return hcat(dat[:,2], wingAngs)
 end
 
-pl1 = scatter(dat[:,2], wingAngs, xlabel="act", ylabel="wing")
-plot(pl1)
+alldata = vcat(transmissionStaticProcessCSV("poke1.csv"), transmissionStaticProcessCSV("poke2.csv"), transmissionStaticProcessCSV("poke3.csv"), transmissionStaticProcessCSV("poke4.csv"))
+
+pl1 = scatter(alldata[:,1], alldata[:,2], xlabel="act", ylabel="tau")
+
+xs = range(1,stop=100, length=50)
+μs = log.(xs)
+σs = rand(length(xs))
+
+pl2 = plot(xs,μs,grid=false,ribbon=σs,fillalpha=.5)
+
+plot(pl1, pl2)
