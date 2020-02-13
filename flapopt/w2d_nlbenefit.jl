fdef83148b1f77f76398a0d30cd11e32bea347e1
Avik De <avikde@gmail.com>
2020-02-13 09:30:58 -0500

    plot starting to work but dataset had error - all Qdt and phi were the same

--- flapopt/w2d_nlbenefit.jl
+++ flapopt/w2d_nlbenefit.jl
@@ -8,7 +8,7 @@ function nonlinBenefit(fname, ret, Tratios, minals, Qdts=[5e3, 1e4], Phis=[90,12
 		i += 1
 		print(i,"/",Ntotal,": ")
 		m.kbo[1] = ko
-		rr = opt1(m, ret["traj"], ret["param"], 1, minal, τ21ratiolim; τ2eq=τ2eq, tol=5e-2, Qdt=Qdt,Φ=phi, kwargs...)
+		rr = opt1(m, ret["traj"], ret["param"], 1, minal, τ21ratiolim; τ2eq=τ2eq, tol=5e-2, Qdt=Qdt, Φ=phi, kwargs...)
 		if rr["status"] >= -2
 			return [rr["u∞"]; rr["al"]; rr["FD∞"]; rr["param"]]
 		else
@@ -26,8 +26,6 @@ function plotNonlinBenefit(fname, ypl; s=100, xpl=[0,3])
 	results = matread(fname)["res"]
 	
 	Nhead = length(size(results))
-	results = results[:,:,1,1,1] # pick Qdt, phi
-	println(size(results))
 
 	function normalizeTbenefit(results)
 		# Row 1 has Tratio=0 (res[1,:])
@@ -43,7 +41,10 @@ function plotNonlinBenefit(fname, ypl; s=100, xpl=[0,3])
 		return results
 	end
 
-	results = normalizeTbenefit(results)
+	# results = results[:,:,1,1,1]
+	# results = normalizeTbenefit(results)
+	results = results[:,3,1,2,:]
+	println(size(results))
 
 	xyzi = zeros(length(results[1,1]),0)
 	for res in results
@@ -52,16 +53,21 @@ function plotNonlinBenefit(fname, ypl; s=100, xpl=[0,3])
 		end
 	end
 	# for plotting
+	head = xyzi[1:Nhead,:]
 	stats = xyzi[Nhead+1:Nhead+4,:]
 	params = xyzi[Nhead+4:end,:]
 	AL = stats[2,:]
 	Tbenefit = stats[1,:]#clamp.(stats[1,:], 0.0, 1.0)#spline fit cause > 1 which doesn't make sense
 	FLspec = AL./stats[3,:]
+	ko = head[5,:]
 
-	X = range(xpl[1], xpl[2], length=50)
-	Y = range(ypl[1], ypl[2], length=50)
+	# SET AXES HERE
 
 	function contourFromUnstructured(xi, yi, zi; title="", rev=false)
+		xpl = minimum(xi), maximum(xi)
+		ypl = minimum(yi), maximum(yi)
+		X = range(xpl..., length=50)
+		Y = range(ypl..., length=50)
 		# Spline from unstructured data https://github.com/kbarbary/Dierckx.jl
 		# println("Total points = ", length(xi))
 		spl = Spline2D(xi, yi, zi; s=s)
@@ -77,13 +83,22 @@ function plotNonlinBenefit(fname, ypl; s=100, xpl=[0,3])
 
 	Tractual = params[5,:]./params[2,:]
     
+	# return [
+	# 	# scatter(xyzi[1,:], xyzi[4,:]),
+	# 	# scatter3d(xyzi[1,:], xyzi[4,:], xyzi[3,:]),
+	# 	contourFromUnstructured(Tractual, AL, Tbenefit; title="Nonlinear transmission benefit [ ]"),
+	# 	contourFromUnstructured(Tractual, AL, FLspec; title="FL sp.", rev=true),
+	# 	# contourFromUnstructured(Tractual, AL, params[2,:]; title="T1 [rad/mm]"),
+	# 	contourFromUnstructured(Tractual, AL, params[6,:]; title="Aw [mm^2]"),
+	# 	contourFromUnstructured(Tractual, AL, 1000.0 ./(N*params[7,:]); title="Freq [Hz]")
+	# ]
 	return [
 		# scatter(xyzi[1,:], xyzi[4,:]),
 		# scatter3d(xyzi[1,:], xyzi[4,:], xyzi[3,:]),
-		contourFromUnstructured(Tractual, AL, Tbenefit; title="Nonlinear transmission benefit [ ]"),
-		contourFromUnstructured(Tractual, AL, FLspec; title="FL sp.", rev=true),
+		# contourFromUnstructured(Tractual, ko, Tbenefit; title="Nonlinear transmission benefit [ ]"),
+		contourFromUnstructured(Tractual, ko, FLspec; title="FL sp.", rev=true),
 		# contourFromUnstructured(Tractual, AL, params[2,:]; title="T1 [rad/mm]"),
-		contourFromUnstructured(Tractual, AL, params[6,:]; title="Aw [mm^2]"),
-		contourFromUnstructured(Tractual, AL, 1000.0 ./(N*params[7,:]); title="Freq [Hz]")
+		contourFromUnstructured(Tractual, ko, params[6,:]; title="Aw [mm^2]"),
+		contourFromUnstructured(Tractual, ko, 1000.0 ./(N*params[7,:]); title="Freq [Hz]")
 	]
 end
