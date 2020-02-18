cfa625dd9b5425444220c01b5830eb167c15427b
Avik De <avikde@gmail.com>
2020-02-18 11:54:22 -0500

    need more data

--- flapopt/w2d_olexp.jl
+++ flapopt/w2d_olexp.jl
@@ -1,46 +1,5 @@
 using Plots, DelimitedFiles
 
-mod4bh1_V = [#= 150, =# 160, 170, 180, 190, 200]
-
-mod4bh1 = [
-	# [150	47.08;
-	# 160	61.52;
-	# 170	75.67;
-	# 175	75.99;
-	# 180	73.98],
-	[150	52.05;
-	160	72.76;
-	170	81.88;
-	175	80.78;
-	180	76.77],
-	[150	57.47;
-	160	82.13;
-	165	84.7;
-	170	86.7;
-	180	78.83],
-	[140	43.96;
-	150	58.41;
-	160	72.68;
-	# 170	64.02;
-	175	86.21;
-	180	82.02;
-	185	79.17;
-	190	73.23],
-	[140	46.51;
-	150	72.35;
-	155	87.65;
-	158	92.36;
-	160	93.17;
-	165	65.21;
-	170	70.31],
-	[140	47.67;
-	150	73.97;
-	155	90.75;
-	157	95.66;
-	160	82.74;
-	170	74.94]
-	]
-
 function readOLExpCSV(fname)
 	dat = readdlm(fname, ',', Float64, skipstart=1)
 	Vs = unique(dat[:,1])
@@ -49,7 +8,7 @@ end
 
 function olExpPlot(V, stroke; showV=[])
 	Nv = length(V)
-	p = plot(xlabel="Freq [Hz]", ylabel="Norm. stroke ampl [deg/V]", ylims=(0.2,0.7))
+	p = plot(xlabel="Freq [Hz]", ylabel="Norm. stroke ampl [deg/V]", ylims=(0.2,0.7), legend=:topleft)
 	for i=1:Nv
 		if length(showV) == 0 || Int(V[i]) in showV
 			plot!(p, stroke[i][:,1], stroke[i][:,2]/V[i], label=Int(V[i]), markershape=:auto, lw=2)
@@ -59,9 +18,10 @@ function olExpPlot(V, stroke; showV=[])
 end
 
 mod1a1_V, mod1a1 = readOLExpCSV("data/normstroke/Param opt manuf 2 - mod1 a1 redo.csv")
+mod4bh1_V, mod4bh1 = readOLExpCSV("data/normstroke/Param opt manuf 2 - mod4 b h1.csv")
 
 plot(
-	olExpPlot(mod1a1_V, mod1a1; showV=[160,170,180,190,200]), 
-	olExpPlot(mod4bh1_V, mod4bh1))
+	olExpPlot(mod1a1_V, mod1a1; showV=[120,160,190]), 
+	olExpPlot(mod4bh1_V, mod4bh1; showV=[120,150,160,170,180,190]))
 gui()
 	
