99882d978f6c5879c8c60fb689ba80811e1d5286
Avik De <avikde@gmail.com>
2020-05-06 08:05:54 -0400

    different control rate OK

--- template/w2d_controlaffine.jl
+++ template/w2d_controlaffine.jl
@@ -1,5 +1,13 @@
 using LinearAlgebra, DifferentialEquations, Plots, OSQP, SparseArrays
 
+# TYPES ----------------------
+
+abstract type ControlAffine end
+struct CAVertical <: ControlAffine end
+struct CAPlanar <: ControlAffine end
+
+# ----------------------------
+
 # control-affine
 function aeroWrench(f, Φ)
 	# params?
@@ -38,7 +46,7 @@ function controlAffinePlanarDynamics(qb, dqb, xwL, xwR)
 	return Mb \ (-h + [rot(qb[3]) * [0;Fz]; Rx])
 end
 
-function controlAffinePlanar(y)
+function nonLinearDynamics(m::CAPlanar, y)
 	# unpack
 	qb = y[1:3]
 	dqb = y[4:6]
@@ -71,7 +79,7 @@ end
 
 nu = 4
 
-function qpSetupDense()
+function qpSetupDense(m::CAPlanar)
 	model = OSQP.Model()
 	# QP solution with only u
 	# with a desired dydes, want 
@@ -86,7 +94,7 @@ function qpSetupDense()
 	return model
 end
 
-function qpSolve(model, fy, gy, vdes)
+function qpSolve(m::CAPlanar, model, fy, gy, vdes)
 	# this is for the 3dof body vel only
 	wb = fill(10.0, 3)
 	Qb = diagm(0 => wb)
@@ -103,42 +111,62 @@ function qpSolve(model, fy, gy, vdes)
 	return res.x
 end
 
+# --- 1D model
+
+"ddz = v; dv = k(vdes - v)"
+function nonLinearDynamics(m::CAVertical, y)
+	k = 1.0 # first order vdot
+	z, dz, v = y
+	# control-affine cts
+	dydt0 = [dz; v; -k * v]
+	dydt1 = k # * vdes (input)
+	return dydt0, dydt1
+end
+
 # ----
-
-# test
-y0 = vcat(zeros(6),0.15,π/2,0.15,π/2)
-ny = length(y0)
-fy, gy = controlAffinePlanar(y0)
-model = qpSetupDense()
-# print(fieldnames(typeof(model.workspace)))
-
-function runSim(y0, tend; simdt=0.02)
+function runSim(ca::ControlAffine, y0, tend, controller; simdt=0.02, udt=0.1)
 	# Functions to evaluate the vector field
-	function getU(fy, gy)
-		vdes = [0,0.1,0]
-		return qpSolve(model, fy, gy, vdes)
-	end
+	lastUUpdate = -Inf
+	Uts = []
+	Us = []
+
 	function vf(y, p, t)
-		fy, gy = controlAffinePlanar(y)
-		# u = [0.15, π/2, 0.15, π/2]
-		return fy + gy * getU(fy, gy)
+		fy, gy = nonLinearDynamics(ca, y)
+
+		if t > lastUUpdate + udt
+			append!(Uts, t)
+			push!(Us, controller(ca, t, y, fy, gy))
+			lastUUpdate = t
+		end
+
+		return fy + gy * Us[end]
 	end
 	# OL traj1
 	teval = collect(0:simdt:tend) # [ms]
 	prob = ODEProblem(vf, y0, (teval[1], teval[end]))
 	sol = solve(prob, saveat=teval)
-	# re-calculate all the inputs
 	yk = hcat(sol.u...) # ny,N
-	N = size(yk,2)
-	uk = zeros(nu, N)
-	for k = 1:N
-		fy, gy = controlAffinePlanar(yk[:,k])
-		uk[:,k] = getU(fy, gy)
-	end
-	return sol.t, yk, uk
+	uk = hcat(Us...)
+	return sol.t, yk, Uts, uk
 end
 
-tt, yy, uu = runSim(y0, 1)
+# test -------------------
+
+cap = CAPlanar()
+y0 = vcat(zeros(6),0.15,π/2,0.15,π/2)
+ny = length(y0)
+fy, gy = nonLinearDynamics(cap, y0)
+# print(fieldnames(typeof(model.workspace)))
+
+function capController(ca, t, y, fy, gy)
+	vdes = [0,0.1,0]
+	return qpSolve(ca, model, fy, gy, vdes)
+	# u = [0.15, π/2, 0.15, π/2]
+end
+
+
+model = qpSetupDense(cap)
+tt, yy, tu, uu = runSim(cap, y0, 1, capController)
 # vf(y0, [], 0)
 
 # Plot
@@ -147,10 +175,10 @@ p2 = plot(tt, yy[1,:], lw=2, xlabel="t", label="y")
 plot!(p2, tt, yy[2,:], lw=2, xlabel="t", label="z")
 p3 = plot(tt, yy[3,:], lw=2, xlabel="t", label="phi")
 # Plot inputs
-pu1 = plot(tt, uu[1,:], lw=2, xlabel="t", label="fL")
-plot!(pu1, tt, uu[3,:], lw=2, xlabel="t", label="fR")
-pu2 = plot(tt, uu[2,:], lw=2, xlabel="t", label="VL")
-plot!(pu2, tt, uu[4,:], lw=2, xlabel="t", label="VR")
+pu1 = plot(tu, uu[1,:], lw=2, xlabel="t", label="fL")
+plot!(pu1, tu, uu[3,:], lw=2, xlabel="t", label="fR")
+pu2 = plot(tu, uu[2,:], lw=2, xlabel="t", label="VL")
+plot!(pu2, tu, uu[4,:], lw=2, xlabel="t", label="VR")
 
 plot(p1, p2, p3, pu1, pu2)
 gui()
