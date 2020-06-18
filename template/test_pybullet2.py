f6965a7a0ff608d78c4cc3717a3c03166d431fd1
Avik De <avikde@gmail.com>
2020-06-18 13:42:22 -0400

    example works

--- template/test_pybullet2.py
+++ template/test_pybullet2.py
@@ -17,14 +17,22 @@ p.setTimeStep(SIM_TIMESTEP)
 # load robot
 startPos = [0,0,0]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-bid2 = p.loadURDF("test2link.urdf", startPos, startOrientation, useFixedBase=True)
+bid = p.loadURDF("test2link.urdf", startPos, startOrientation, useFixedBase=True)
 
-simt = 0
+# springy control for both joints
+# p.resetJointState(bid, 0, 0.5, 0)
+p.resetJointState(bid, 1, -0.5, 0)
+p.setJointMotorControlArray(bid, [0,1], p.POSITION_CONTROL, targetPositions=[0,0], positionGains=[0.01,1], velocityGains=[0.1,0.99])
 
-idff = p.addUserDebugParameter("Test force", -1, 1, 0)
+idff = p.addUserDebugParameter("Test force", -0.01, 0.01, 0)
 
 while True:
     try:
+        # Apply force to last link
+        pend = [0,1.1,0.1]
+        Fend = [p.readUserDebugParameter(idff),0,0]
+        p.applyExternalForce(bid, 1, Fend, pend, p.WORLD_FRAME)
+        p.addUserDebugLine(pend, np.array(pend) + 100*np.array(Fend), lineColorRGB=[1,0,0],lifeTime=0.1,lineWidth=2)
         p.stepSimulation()
         time.sleep(SIM_TIMESTEP * 1e-3)
     except:
