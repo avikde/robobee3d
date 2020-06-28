05fe53c60c7771c14e9619ba87bcb7e1f7e51900
Avik De <avikde@gmail.com>
2020-06-28 17:40:42 -0400

    key by voltage

--- util/staticTestData.py
+++ util/staticTestData.py
@@ -1,18 +1,33 @@
 import csv, sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 def loadStaticTestData(fname):
     csvfile = open(fname, newline='')
     dat = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    # convert to lists for each key
-    voltage = []
-    freq = []
-    strokes = []
+    # create dict with voltage as key
+    freq = {}
+    strokes = {}
+    # create keys
     for row in dat:
-        voltage.append(float(row['Voltage']))
-        freq.append(float(row['Frequency']))
-        strokes.append(np.hstack((float(row['Stroke bot']), float(row['Stroke bot']))))
-    return np.array(voltage), np.array(freq), np.array(strokes)
+        freq[row['Voltage']] = []
+        strokes[row['Voltage']] = []
+    # populate the data
+    csvfile.seek(0)
+    dat = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    for row in dat:
+        freq[row['Voltage']].append(float(row['Frequency']))
+        strokes[row['Voltage']].append(np.hstack((float(row['Stroke bot']), float(row['Stroke bot']))))
+    # convert to np array from list
+    for key in freq.keys():
+        freq[key] = np.array(freq[key])
+        strokes[key] = np.array(strokes[key])
+    return freq, strokes
 
-V, f, S = loadStaticTestData(sys.argv[1])
-print(V, f, S)
+# def normStrokePlot(ax, V, f, S):
+#     ax.
+
+f,S = loadStaticTestData(sys.argv[1])
+print(f, S)
+
+
