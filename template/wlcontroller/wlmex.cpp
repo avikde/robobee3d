6813f22276c4810fa0d33d150d45594e0705df0d
Avik De <avikde@gmail.com>
2020-08-14 12:27:49 -0400

    adding mex...works

---
+++ template/wlcontroller/wlmex.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file wlmex.cpp
+ * @author Avik De (avikde@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-08-14
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <mex.h>
+#include "wlcontroller.hpp"
