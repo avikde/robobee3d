67cccb92fcfbcfba8457e0311c3a7581289653d2
Avik De <avikde@gmail.com>
2020-08-26 18:23:39 -0400

    create mex to test

--- template/wlcontroller/wltest.m
+++ template/wlcontroller/wltest.m
@@ -1,17 +1,18 @@
 function wltest()
 
 % repeatedly call for a made-up pdes to test
-aa = zeros(100,3);
-for i = 1:100
+Nn=500;
+aa = zeros(Nn,3);
+for i = 1:Nn
 	[drv_amp, drv_pch, drv_roll] = wlwrap(zeros(6), [0,0,10,0,0,0]);
 	aa(i,:) = [drv_amp, drv_pch, drv_roll];
 end
 
 figure(1)
-for i = 1:3
-	subplot(3,1,i)
-	plot(aa(:,i))
-end
+subplot(2,1,1)
+plot(aa(:,1))
+subplot(2,1,2)
+plot(aa(:,2:3))
 
 end
 
@@ -29,13 +30,10 @@ end
 mb = 100;
 g = 9.81e-3;
 h0 = [0, 0, mb * g, 0, 0, 0];
-p0 = zeros(1,6);
-% pdes = [0, 0, 20, 0, 0, 0];
-Qdiag = [1,1,1,0.1,0.1,0.1];
-kpmom = [0,0,1,0.1,0.1,0.1];
+pddes = [0, 0, 10, 0,0,0];
 
 % Update
-u = wlmex(u0, p0, h0, pdes, kpmom, Qdiag);
+u = wlControllerUpdate(single(u0), single(h0), single(pddes));
 
 % Convert to robot coords. u = [Vmean, uoffs, udiff, h2]. uoffs, udiff
 % normalized by Vmean
