e98d340487eb850f0a2895e8723aa644bd94a258
Avik De <avikde@gmail.com>
2020-09-25 19:18:52 -0400

    codegen; moved files

--- template/genqp.py
+++ template/genqp.py
@@ -15,7 +15,7 @@ def qpSetupDense(n, m):
     model.setup(P=P, q=q, A=A, l=l, u=u, eps_rel=1e-4, eps_abs=1e-4, verbose=False)
     return model
 
-def mpcDirtran(m, N, dt, snom, y0):
+def mpcDirtran(m, N, dt, snom, y0, Qfdiag, ydes):
     """See https://github.com/avikde/robobee3d/pull/181.
     snom should be an N, shaped array"""
     nq = 2 #q = (p,s)
@@ -44,16 +44,31 @@ def mpcDirtran(m, N, dt, snom, y0):
         if k == 0:
             c[k*ny:(k+1)*ny] = Ad @ np.asarray(y0)
     A = sp.csc_matrix(A)
-    print(A, c)
+    # print(A, c)
+
+    # construct objective. 
+    P = np.zeros((nx, nx))
+    q = np.zeros(nx)
+    # only final cost
+    for i in range(ny):
+        P[nx - ny + i, nx - ny + i] = Qfdiag[i]
+    P = sp.csc_matrix(P)
+    q[-ny:] = -np.asarray(ydes)
+
+    # osqp
+    model = osqp.OSQP()
+    model.setup(P=P, q=q, A=A, l=c, u=c, eps_rel=1e-4, eps_abs=1e-4, verbose=False)
+    return model
 
 if __name__ == "__main__":
     # # WLQP gen
     # prob = qpSetupDense(4,4)
+
+    prob = mpcDirtran(100, 3, 2, [0.1, 0.2, 0.3], [1, 0.1, 0, 0], [1, 0.1, 1e-3, 1e-3], [2, 0.2, 0, 0])
+    
     # # codegen
     # try:
     #     prob.codegen('gen', project_type='', force_rewrite=True, parameters='matrices', FLOAT=True, LONG=False)
     # except:
     #     # No worries if python module failed to compile
     #     pass
-
-    mpcDirtran(100, 3, 2, [0.1, 0.2, 0.3], [1, 0.1, 0, 0])
