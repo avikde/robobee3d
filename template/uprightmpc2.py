5d5c9447b0247b5192661827bfddaeac973dc27b
Avik De <avikde@gmail.com>
2020-10-17 20:14:19 -0400

    WIP test dynamics constraint function

--- template/uprightmpc2.py
+++ template/uprightmpc2.py
@@ -48,6 +48,44 @@ def initConstraint():
     
     return sp.csc_matrix(A)
 
+def updateConstraint(A, R):
+    pass
+
+def testDynamicsConstraint():
+    ys = np.zeros((N,ny))
+    dys = np.zeros((N,ny))
+    us = np.random.rand(N,nu)
+
+    y0 = np.random.rand(ny)
+    dy0 = np.random.rand(ny)
+    T0 = 1
+    dt = 1
+    
+    # Basic constituents of dynamics A0, B0 (only sparsity matters)
+    B0 = np.block([
+        [np.ones((3,1)),np.zeros((3,2))],
+        [np.zeros((3,1)),np.ones((3,2))]
+    ])
+    A0 = np.zeros((6, 6))
+    A0[:3,3:] = np.eye(3)
+    c0 = np.ones(ny) # FIXME:
+
+    y1 = y0 + dt * dy0
+    yy = np.copy(y1)
+    dyy = np.copy(dy0)
+    for k in range(N):
+        yy1 = yy + dt * dyy
+        dyy1 = dyy + (A0 @ yy + B0 @ us[k,:] + c0)
+
+        dys[k,:] = dyy1
+        ys[k,:] = yy1
+
+        yy = yy1
+        dyy = dyy1
+
+    # TODO: stack
+
 if __name__ == "__main__":
     A = initConstraint()
     print(A)
+    testDynamicsConstraint()
