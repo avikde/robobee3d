5b2604be93629ef497eb571e5560d1bf52077dec
Avik De <avikde@gmail.com>
2020-10-22 09:41:20 -0400

    garbage

--- template/uprightmpc2/osqp/uprightmpc2.c
+++ template/uprightmpc2/osqp/uprightmpc2.c
@@ -16,7 +16,8 @@
 #include <string.h>
 
 static void PRINTVEC(const float *y, int sz) {
-	for (int i = 0; i < sz; ++i) {
+	int i;
+	for (i = 0; i < sz; ++i) {
 		printf("%.2f,", y[i]);
 	}
 	printf("\n");
@@ -24,11 +25,13 @@ static void PRINTVEC(const float *y, int sz) {
 
 void umpcInit(UprightMPC_t *up, float dt, float g, const float smin[/* 3 */], const float smax[/* 3 */], float TtoWmax, float ws, float wds, float wpr, float wpf, float wvr, float wvf, float wthrust, float wmom, const float Ib[/* 3 */], int maxIter) {
 	static float Ibi[9];
+	int i, k, n1, n2, offs;
+
 	up->dt = dt;
 	up->g = g;
 	up->Tmax = TtoWmax * g;
 	// Weights
-	for (int i = 0; i < 3; ++i) {
+	for (i = 0; i < 3; ++i) {
 		up->Qyr[i] = wpr;
 		up->Qyf[i] = wpf;
 		up->Qyr[3 + i] = up->Qyf[3 + i] = ws;
@@ -39,13 +42,13 @@ void umpcInit(UprightMPC_t *up, float dt, float g, const float smin[/* 3 */], co
 	up->R[0] = wthrust;
 	up->R[1] = up->R[2] = wmom;
 	// Limits
-	for (int i = 0; i < 3; ++i) {
+	for (i = 0; i < 3; ++i) {
 		up->smin[i] = smin[i];
 		up->smax[i] = smax[i];
 	}
 
 	// Other updates
-	for (int i = 0; i < UMPC_NY; ++i) {
+	for (i = 0; i < UMPC_NY; ++i) {
 		up->c0[i] = i == 2 ? -up->g : 0;
 	}
 	up->T0 = 0;
@@ -56,7 +59,7 @@ void umpcInit(UprightMPC_t *up, float dt, float g, const float smin[/* 3 */], co
 	up->e3h[3] = -1;
 
 	// e3h*Ibi calculation
-	for (int i = 0; i < 9; ++i) {
+	for (i = 0; i < 9; ++i) {
 		Ibi[i] = 0;
 	}
 	Ibi[0] = 1.0f / Ib[0];
@@ -64,18 +67,18 @@ void umpcInit(UprightMPC_t *up, float dt, float g, const float smin[/* 3 */], co
 	Ibi[8] = 1.0f / Ib[2];
 	matMult(up->e3hIbi, up->e3h, Ibi, 3, 3, 3, 1.0f, 0, 0); // e3h*Ibi
 
-	for (int i = 0; i < UMPC_NX; ++i) {
+	for (i = 0; i < UMPC_NX; ++i) {
 		up->q[i] = 0;
 	}
-	for (int i = 0; i < UMPC_NC; ++i) {
+	for (i = 0; i < UMPC_NC; ++i) {
 		up->l[i] = up->u[i] = 0;
 	}
 
 	// Ax idx ---
 	// Left third, T0*dt
-	int offs = 0;
-	int n2 = 2*UMPC_NY + 6; // nnz in each block col on the left
-	for (int k = 0; k < UMPC_N-2; ++k) {
+	offs = 0;
+	n2 = 2*UMPC_NY + 6; // nnz in each block col on the left
+	for (k = 0; k < UMPC_N-2; ++k) {
 		up->Ax_idx[offs+0] = n2*k + 8;
 		up->Ax_idx[offs+1] = n2*k + 12;
 		up->Ax_idx[offs+2] = n2*k + 16;
@@ -84,9 +87,9 @@ void umpcInit(UprightMPC_t *up, float dt, float g, const float smin[/* 3 */], co
 	up->nAxT0dt = offs;
 
 	// Middle third, dt
-	int n1 = (2*UMPC_N-1)*UMPC_NY + (UMPC_N-2)*3 + 3*UMPC_N; // All the nnz in the left third
+	n1 = (2*UMPC_N-1)*UMPC_NY + (UMPC_N-2)*3 + 3*UMPC_N; // All the nnz in the left third
 	n2 = 3*UMPC_NY; // nnz in each of the first N-1 block cols in the middle third
-	for (int k = 0; k < UMPC_N; ++k) {
+	for (k = 0; k < UMPC_N; ++k) {
 		up->Ax_idx[offs+0] = n1 + n2*k + 0;
 		if (k < UMPC_N-1) {
 			up->Ax_idx[offs+1] = n1 + n2*k + 3;
@@ -109,14 +112,14 @@ void umpcInit(UprightMPC_t *up, float dt, float g, const float smin[/* 3 */], co
 	n1 += 3*UMPC_NY*(UMPC_N-1) + 2*UMPC_NY; // All the nnz in the left, middle third
 	n2 = 10; // nnz in each B0 + 1 for thrust lim
 	// s0
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < 3; ++i)
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < 3; ++i)
 			up->Ax_idx[offs+i] = n1 + n2*k + i;
 		offs += 3;
 	}
 	// Btau
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < 6; ++i)
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < 6; ++i)
 			up->Ax_idx[offs+i] = n1 + n2*k + 4 + i;
 		offs += 6;
 	}
@@ -134,21 +137,22 @@ static float A0_times_i(const UprightMPC_t *up, const float y[/* ny */], int i) 
 // Same s0, Btau for each k (that's what ended up happening in python anyway)
 static void umpcUpdateConstraint(UprightMPC_t *up, const float s0[/*  */], const float Btau[/*  */], const float y0[/*  */], const float dy0[/*  */]) {
 	static float y1[UMPC_NY];
+	int i, k, offs;
 	
 	// Update vector ---
-	for (int i = 0; i < UMPC_N * UMPC_NY; ++i) {
+	for (i = 0; i < UMPC_N * UMPC_NY; ++i) {
 		up->l[i] = 0;
 	}
 	// y1 = y0 + dt * dy0
-	for (int i = 0; i < UMPC_NY; ++i) {
+	for (i = 0; i < UMPC_NY; ++i) {
 		y1[i] = y0[i] + up->dt * dy0[i];
 		// l[:ny] = -y1
 		up->l[i] = -y1[i];
 	}
 	// Dynamics constraints
-	for (int k = 0; k < UMPC_N; ++k) {
+	for (k = 0; k < UMPC_N; ++k) {
 		float *pk = &up->l[UMPC_NY * (UMPC_N + k)];
-		for (int i = 0; i < UMPC_NY; ++i) {
+		for (i = 0; i < UMPC_NY; ++i) {
 			if (k == 0) {
 				pk[i] = -dy0[i] - up->dt * A0_times_i(up, y0, i) - up->dt * up->c0[i];
 			} else if (k == 1) {
@@ -159,37 +163,37 @@ static void umpcUpdateConstraint(UprightMPC_t *up, const float s0[/*  */], const
 		}
 	}
 	// copy for dynamics
-	for (int i = 0; i < 2 * UMPC_N * UMPC_NY; ++i) {
+	for (i = 0; i < 2 * UMPC_N * UMPC_NY; ++i) {
 		up->u[i] = up->l[i];
 	}
 	// s lims
-	for (int k = 0; k < UMPC_N; ++k) {
+	for (k = 0; k < UMPC_N; ++k) {
 		float *pl = &up->l[2*UMPC_N*UMPC_NY + 3*k];
 		float *pu = &up->u[2*UMPC_N*UMPC_NY + 3*k];
-		for (int i = 0; i < 3; ++i) {
+		for (i = 0; i < 3; ++i) {
 			pl[i] = up->smin[i];
 			pu[i] = up->smax[i];
 		}
 	}
 	// thrust lims
-	for (int k = 0; k < UMPC_N; ++k) {
+	for (k = 0; k < UMPC_N; ++k) {
 		up->l[2*UMPC_N*UMPC_NY + 3*UMPC_N + k] = -up->T0;
 		up->u[2*UMPC_N*UMPC_NY + 3*UMPC_N + k] = up->Tmax - up->T0;
 	}
 
 	// Update matrix ---
-	for (int i = 0; i < up->nAxdt; ++i) {
+	for (i = 0; i < up->nAxdt; ++i) {
 		up->Ax_data[i] = i < up->nAxT0dt ? up->dt * up->T0 : up->dt;
 	}
-	int offs = up->nAxdt;
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < 3; ++i) {
+	offs = up->nAxdt;
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < 3; ++i) {
 			up->Ax_data[offs + i] = up->dt * s0[i];
 		}
 		offs += 3;
 	}
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < 6; ++i) {
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < 6; ++i) {
 			up->Ax_data[offs + i] = up->dt * Btau[i];
 		}
 		offs += 6;
@@ -197,24 +201,25 @@ static void umpcUpdateConstraint(UprightMPC_t *up, const float s0[/*  */], const
 }
 
 static void updateObjective(UprightMPC_t *up, const float ydes[/* 6 */], const float dydes[/* 6 */]) {
+	int offs, k, i;
 	// q, P diag ---
-	int offs = 0;
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < UMPC_NY; ++i) {
+	offs = 0;
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < UMPC_NY; ++i) {
 			up->Px_data[offs + i] = k == UMPC_N-1 ? up->Qyf[i] : up->Qyr[i];
 			up->q[offs + i] = -up->Px_data[offs + i] * ydes[i];
 		}
 		offs += UMPC_NY;
 	}
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < UMPC_NY; ++i) {
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < UMPC_NY; ++i) {
 			up->Px_data[offs + i] = k == UMPC_N-1 ? up->Qdyf[i] : up->Qdyr[i];
 			up->q[offs + i] = -up->Px_data[offs + i] * dydes[i];
 		}
 		offs += UMPC_NY;
 	}
-	for (int k = 0; k < UMPC_N; ++k) {
-		for (int i = 0; i < UMPC_NU; ++i) {
+	for (k = 0; k < UMPC_N; ++k) {
+		for (i = 0; i < UMPC_NU; ++i) {
 			up->Px_data[offs + i] = up->R[i];
 			// Last rows of q remain 0
 		}
@@ -225,6 +230,7 @@ static void updateObjective(UprightMPC_t *up, const float ydes[/* 6 */], const f
 int umpcUpdate(UprightMPC_t *up, float uquad[/* 3 */], float accdes[/* 6 */], const float p0[/* 3 */], const float R0[/* 9 */], const float dq0[/* 6 */], const float pdes[/* 3 */], const float dpdes[/* 3 */]) {
 	static float s0[3], ds0[3], y0[UMPC_NY], dy0[UMPC_NY], ydes[UMPC_NY], dydes[UMPC_NY], dummy[9], Btau[9];
 	static float dy1des[UMPC_NY], dq1des[UMPC_NY];
+	int i, ret;
 
 	// Compute some states
 	memcpy(s0, &R0[6], 3 * sizeof(float)); // column major R0, and want third col
@@ -234,7 +240,7 @@ int umpcUpdate(UprightMPC_t *up, float uquad[/* 3 */], float accdes[/* 6 */], co
 	// Btau = (-R0 @ e3h @ self.Ibi)[:,:2]
 	matMult(Btau, R0, up->e3hIbi, 3, 3, 3, -1.0f, 0, 0); // -R0*e3h*Ibi; col major => can take first 6 elems
 	
-	for (int i = 0; i < UMPC_NY; ++i) {
+	for (i = 0; i < UMPC_NY; ++i) {
 		// y0 = np.hstack((p0, s0))
 		y0[i] = i < 3 ? p0[i] : s0[i - 3];
 		// dy0 = np.hstack((dq0[:3], ds0))
@@ -257,16 +263,16 @@ int umpcUpdate(UprightMPC_t *up, float uquad[/* 3 */], float accdes[/* 6 */], co
 	osqp_update_bounds(&workspace, up->l, up->u);
 	osqp_update_lin_cost(&workspace, up->q);
 	osqp_update_P_A(&workspace, up->Px_data, OSQP_NULL, UMPC_NX, up->Ax_data, up->Ax_idx, UMPC_nAdata);
-	int ret = osqp_solve(&workspace);
+	ret = osqp_solve(&workspace);
 
 	// copy solution
-	for (int i = 0; i < UMPC_NU; ++i) {
+	for (i = 0; i < UMPC_NU; ++i) {
 		uquad[i] = workspace.solution->x[2*UMPC_NY*UMPC_N + i];
 	}
 	up->T0 += uquad[0];
 	uquad[0] = up->T0;
 
-	for (int i = 0; i < UMPC_NY; ++i) {
+	for (i = 0; i < UMPC_NY; ++i) {
 		dy1des[i] = workspace.solution->x[UMPC_NY*UMPC_N + i];
 		if (i < 3)
 			dq1des[i] = dy1des[i];
@@ -274,7 +280,7 @@ int umpcUpdate(UprightMPC_t *up, float uquad[/* 3 */], float accdes[/* 6 */], co
 	matMult(dummy, R0, &dy1des[3], 3, 1, 3, 1.0f, 1, 0); // R0^T*s1des
 	matMult(&dq1des[3], up->e3h, dummy, 3, 1, 3, 1.0f, 0, 0); // e3h*R0^T*s1des
 
-	for (int i = 0; i < UMPC_NY; ++i) {
+	for (i = 0; i < UMPC_NY; ++i) {
 		accdes[i] = (dq1des[i] - dq0[i]) / up->dt;
 	}
 
