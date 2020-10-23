c2cbf58f1177f573aea9ef9ece068af7529c590c
Avik De <avikde@gmail.com>
2020-10-23 08:03:50 -0400

    update lct spec

--- template/uprightmpc2/legacy_code_gen.m
+++ template/uprightmpc2/legacy_code_gen.m
@@ -2,7 +2,7 @@ def = legacy_code('initialize');
 def.SFunctionName = 'umpcS';
 
 % Is column major
-def.OutputFcnSpec = 'void umpcS(single y1[3], single y2[6], single u1[6], single u2[3][3], single u3[6], single u4[3], single u5[3], single u6, single u7, single u8[3], single u9[3], single u10, single u11, single u12, single u13, single u14, single u15, single u16, single u17, single u18, single u19[3], int32 u20)';
+def.OutputFcnSpec = 'void umpcS(single y1[3], single y2[6], single u1[3], single u2[3][3], single u3[6], single u4[3], single u5[3], single u6, single u7, single u8, single u9, single u10, single u11, single u12, single u13, single u14, single u15, single u16, single u17, single u18[3], single u19[4], single u20[4], single u21[4], single u22[6], single u23, int32 u24)';
 
 def.HeaderFiles = {'uprightmpc2.h'};
 def.SourceFiles = {'uprightmpc2.c', 'eigenc.c', ...
@@ -20,7 +20,7 @@ def.Options.language = 'C';
 
 legacy_code('sfcn_cmex_generate', def);
 
-%a
+%
 
 %legacy_code('compile', def);
 %legacy_code('slblock_generate', def);
