9c3c5466dbf3311a823f3f088a74787d009c15c6
Avik De <avikde@gmail.com>
2020-11-18 09:34:28 -0500

    add init; need to rebuild with VFP settings

--- g4bee/Src/main.c
+++ g4bee/Src/main.c
@@ -51,7 +51,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void loopUpdate();
+void setup();
+void loop();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -89,7 +90,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  setup();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -99,7 +100,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    loopUpdate();
+    loop();
   }
   /* USER CODE END 3 */
 }
--- g4bee/app/loop_update.cpp
+++ g4bee/app/loop_update.cpp
@@ -13,13 +13,38 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// Definitions
 #define micros() (HAL_GetTick()*1000 + 1000 - SysTick->VAL*1000000/SystemCoreClock)
+// Globals
+UprightMPC_t up;
 
-extern "C" void loopUpdate() {
+extern "C" void setup() {
+	// Parameters
+	float dt = 5;
+	float g = 9.81e-3f;
+	float TtoWmax = 3;
+	float ws = 1e1;
+	float wds = 1e3;
+	float wpr = 1;
+	float wvr = 1e3;
+	float wpf = 5;
+	float wvf = 2e3;
+	float wthrust = 1e-1;
+	float wmom = 1e-2;
+	float Ib[3] = {3333,3333,1000};
+	int maxIter = 10;
+	// Call init
+	umpcInit(&up, dt, g, TtoWmax, ws, wds, wpr, wpf, wvr, wvf, wthrust, wmom, Ib, maxIter);
+}
+
+extern "C" void loop() {
 	uint32_t t1 = micros();
-	HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+
+	// upri
+
 	uint32_t t2 = micros();
 	
+	HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 	printf("hello %lu %lu\n", HAL_GetTick(), t2 - t1);
 	HAL_Delay(100);
 }
