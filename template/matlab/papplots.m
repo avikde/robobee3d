550c638694706207450c9accdfd2bd6ac357a7f0
Avik De <avikde@gmail.com>
2020-12-28 16:09:48 -0500

    final for plots in draft

--- template/matlab/papplots.m
+++ template/matlab/papplots.m
@@ -5,18 +5,21 @@ figure
 clf
 ax1 = gca();
 
-figure('position', [0,0,600,250])
+fig1 = figure('position', [0,0,600,300])
+fig1.Renderer='Painters';
 clf
-ax2 = subplot(1,2,1);
-ax3 = subplot(1,2,2);
+ax2 = subplot(2,2,1);
+ax3 = subplot(2,2,2);
+ax4 = subplot(2,2,3);
+ax5 = subplot(2,2,4);
 
-plotTrial(ax1, ax2, ax3, '../../../../Desktop/umpc_mm_data/trial36.mat', 1.5, [0,0,1])
+plotTrial(ax1, ax2, ax3, ax4, ax5, '../../../../Desktop/umpc_mm_data/trial36.mat', 1.5, [0,0,1])
 hold(ax1, 'all')
-plotTrial(ax1, ax2, ax3, '../../../../Desktop/umpc_mm_data/trial37.mat', 1.6, [1,0,0]) % 2
-plotTrial(ax1, ax2, ax3, '../../../../Desktop/umpc_mm_data/trial32.mat', 1.6, [0,0.5,0]) % 1.9
-plotTrial(ax1, ax2, ax3, '../../../../Desktop/umpc_mm_data/trial31.mat', 1.4, [0.5,0.5,0])
-plotTrial(ax1, ax2, ax3, '../../../../Desktop/umpc_mm_data/trial21.mat', 1.5, [0,0.5,0.5])
-plotTrial(ax1, ax2, ax3, '../../../../Desktop/umpc_mm_data/trial20.mat', 1.4, [0.5,0,0.5])
+plotTrial(ax1, ax2, ax3, ax4, ax5, '../../../../Desktop/umpc_mm_data/trial37.mat', 1.6, [1,0,0]) % 2
+plotTrial(ax1, ax2, ax3, ax4, ax5, '../../../../Desktop/umpc_mm_data/trial32.mat', 1.5, [0,0.5,0]) % 1.9
+plotTrial(ax1, ax2, ax3, ax4, ax5, '../../../../Desktop/umpc_mm_data/trial31.mat', 1.4, [0.5,0.5,0])
+plotTrial(ax1, ax2, ax3, ax4, ax5, '../../../../Desktop/umpc_mm_data/trial21.mat', 1.5, [0,0.5,0.5])
+plotTrial(ax1, ax2, ax3, ax4, ax5, '../../../../Desktop/umpc_mm_data/trial20.mat', 1.4, [0.5,0,0.5])
 
 hold(ax1, 'off')
 grid(ax1, 'on')
@@ -27,16 +30,18 @@ xlabel(ax1, 'x [m]')
 ylabel(ax1, 'y [m]')
 zlabel(ax1, 'z [m]')
 
-plot(ax2, [0,2], [0,0], 'k--')
-ylim(ax2, [-pi/4, pi/4])
-plot(ax3, [0,2], [0,0], 'k--')
-ylim(ax3, [-pi/4, pi/4])
-ylabel(ax2, 'Roll [rad]')
-ylabel(ax3, 'Pitch [rad]')
-xlabel(ax2, 'Time [s]')
-xlabel(ax3, 'Time [s]')
+plot(ax4, [0,2], [0,0], 'k--')
+ylim(ax4, [-pi/4, pi/4])
+plot(ax5, [0,2], [0,0], 'k--')
+ylim(ax5, [-pi/4, pi/4])
+ylabel(ax2, 'z [m]')
+ylabel(ax3, 'sz [ ]')
+ylabel(ax4, 'Roll [rad]')
+ylabel(ax5, 'Pitch [rad]')
+xlabel(ax4, 'Time [s]')
+xlabel(ax5, 'Time [s]')
 
-function plotTrial(ax1, ax2, ax3, fname, tmax, col)
+function plotTrial(ax1, ax2, ax3, ax4, ax5, fname, tmax, col)
 	%dt = 1/5000;
 	load(fname);
 	tt = yout(:,1);
@@ -48,5 +53,5 @@ function plotTrial(ax1, ax2, ax3, fname, tmax, col)
 	%quickPlots(yout, sampling_time, tmax)
 	[s, ds] = uprightCalcs(qvicon(:,4:6), dqlog(:,4:6));
 	
-	traj3plot(ax1, ax2, ax3, tt, qvicon, s, tmax, col)
+	traj3plot(ax1, ax2, ax3, ax4, ax5, tt, qvicon, s, tmax, col)
 end
--- template/matlab/traj3plot.m
+++ template/matlab/traj3plot.m
@@ -1,4 +1,4 @@
-function traj3plot(ax, ax2, ax3, tt, q, ss, tmax, col)
+function traj3plot(ax, ax2, ax3, ax4, ax5, tt, q, ss, tmax, col)
 
 xyz = q(:,1:3);
 ii = find(tt < tmax & tt > 0.01);
@@ -13,9 +13,13 @@ quiver3(ax, xyz(jj,1), xyz(jj,2), xyz(jj,3), ss(jj,1), ss(jj,2), ss(jj,3), scale
 
 
 % traj
-plot(ax2, tt(ii), q(ii,4))
+plot(ax2, tt(ii), xyz(ii,3))
 hold(ax2, 'on')
-plot(ax3, tt(ii), q(ii,5))
+plot(ax3, tt(ii), 100*ss(ii,3)) % undo manual scaling
 hold(ax3, 'on')
+plot(ax4, tt(ii), q(ii,4))
+hold(ax4, 'on')
+plot(ax5, tt(ii), q(ii,5))
+hold(ax5, 'on')
 
 end
